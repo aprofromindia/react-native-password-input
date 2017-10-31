18840463a89b6f1571b50a788090241971f98a70
Apro <aprofromuk-mail@yahoo.co.in>
2017-10-31 12:46:41 +0100

    add few UT + eslint env setup.

--- __tests__/src/views/PasswordTextInput-test.js
+++ __tests__/src/views/PasswordTextInput-test.js
@@ -7,21 +7,25 @@ describe('PasswordTextInput Snapshots', () => {
   const wrapper = shallow(<PasswordTextInput placeholder="PASSWORD" />);
 
   it('should be empty', () => {
+    expect(wrapper.state('text')).toBe('');
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should store password', () => {
     wrapper.find('TextInput').simulate('changeText', 'password123');
+    expect(wrapper.state().text).toBe('password123');
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should show password when clicked', () => {
     wrapper.find('TouchableOpacity').simulate('press');
+    expect(wrapper.find('TextInput').prop('secureTextEntry')).toBeFalsy();
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should hide password when clicked again', () => {
     wrapper.find('TouchableOpacity').simulate('press');
+    expect(wrapper.find('TextInput').prop('secureTextEntry')).toBeTruthy();
     expect(wrapper).toMatchSnapshot();
   });
 });
